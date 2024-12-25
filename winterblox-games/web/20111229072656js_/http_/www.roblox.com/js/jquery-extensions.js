a6a4297c690497a9566d7218f7f8cb55ff2e7a7b
WinterBlox-Revival <luisgamercool2369420@gmail.com>
2024-12-25 17:59:00 +0200

    Add files via upload

---
+++ winterblox-games/web/20111229072656js_/http_/www.roblox.com/js/jquery-extensions.js
@@ -0,0 +1,40 @@
+﻿var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+﻿jQuery.extend(jQuery.expr[':'], {
+    focus: function(element)
+    {
+        return element == document.activeElement;
+    }
+});﻿
+
+}
+/*
+     FILE ARCHIVED ON ﻿07:26:56 Dec 29, 2011﻿ AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON ﻿15:02:57 Dec 24, 2024﻿.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+﻿playback timings (ms):
+﻿  ﻿captures_list﻿: ﻿0.567﻿
+﻿  ﻿exclusion.robots﻿: ﻿0.023﻿
+﻿  ﻿exclusion.robots.policy﻿: ﻿0.014﻿
+﻿  ﻿esindex﻿: ﻿0.01﻿
+﻿  ﻿cdx.remote﻿: ﻿2.907﻿
+﻿  ﻿LoadShardBlock﻿: ﻿382.471﻿ (﻿6﻿)
+﻿  ﻿PetaboxLoader3.datanode﻿: ﻿418.148﻿ (﻿7﻿)
+﻿  ﻿load_resource﻿: ﻿190.716﻿
+﻿  ﻿PetaboxLoader3.resolve﻿: ﻿92.676﻿
+﻿*/
